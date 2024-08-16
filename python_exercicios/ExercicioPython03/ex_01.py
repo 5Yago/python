276c7c53255664b5a194f50b27ae5d1a5bbb1222
Yago <senaiyago44@gmail.com>
2024-08-16 14:08:52 -0300

    ate-a-5

---
+++ python_exercicios/ExercicioPython03/ex_01.py
@@ -0,0 +1,10 @@
+# Lista usando laço for
+print (" ")
+print ("Lista de Times")
+print (" ")
+
+times = ["Corinthians", "São Paulo", "Fluminense", "Vasco da Gama"]
+
+#listar usando laço
+for time in times:
+    print(time)
